b14fdcc268c4dc383f9fe9414f7432ed37abb44d
Teddy2M6 <47573307+Teddy2M6@users.noreply.github.com>
2019-02-20 12:20:30 -0500

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,23 @@
+-----------------------------------------------------------------------------------------
+--
+-- how to add a text field and button, so that you can enter text
+-- 
+-----------------------------------------------------------------------------------------
+
+local answerTextField = native.newTextField( display.contentCenterX, display.contentCenterY + 200, 450, 75 )
+answerTextField.id = "answer textField"
+
+local enterButton = display.newImageRect( "./myAssests/Sprite/enter.png", 406, 157 )
+enterButton.x = display.contentCenterX
+enterButton.y = display.contentCenterY
+enterButton.id = "enter button"
+display.setDefault("background", 0, 255, 0)
+ 
+local function enterButtonTouch( event )
+    -- it is not perfect, but we will make it better soon
+    print( answerTextField.text )
+
+    return true
+end
+
+enterButton:addEventListener( "touch", enterButtonTouch )
